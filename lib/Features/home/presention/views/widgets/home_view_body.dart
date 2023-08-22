6f465ad01d89f7f91038bcea78ef609dc9e900e3
baraa1999 <baraaalian7@gmail.com>
2023-08-22 13:47:47 +0300

    Best seller item part 3

--- lib/Features/home/presention/views/widgets/home_view_body.dart
+++ lib/Features/home/presention/views/widgets/home_view_body.dart
@@ -4,6 +4,7 @@ import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'best_seller_list_view_item.dart';
 import 'custom_app_bar.dart';
 import 'custom_list_view_item.dart';
 import 'featured_list_view.dart';
@@ -31,51 +32,6 @@ class HomeViewBody extends StatelessWidget {
             height: 20,
           ),
           BestSellerListViewItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              width: 100,
-              height: MediaQuery.of(context).size.height * .25,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(AssetsData.testImage),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Column(
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child: const Text(
-                  'Harry Potter and The Gobalt of Fire',
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20,
-                ),
-              ),
-            ],
-          )
         ],
       ),
     );
