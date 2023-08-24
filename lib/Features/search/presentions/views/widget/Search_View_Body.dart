29b52df37aad75d69cd7d020a5449512226170de
baraa1999 <baraaalian7@gmail.com>
2023-08-24 15:07:46 +0300

    search view done

--- lib/Features/search/presentions/views/widget/Search_View_Body.dart
+++ lib/Features/search/presentions/views/widget/Search_View_Body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly/Features/home/presention/views/widgets/best_seller_list_view_item.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -13,8 +15,37 @@ class SearchViewBody extends StatelessWidget {
       child: Column(
         children: [
           CustomSearchTextField(),
+          SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Search Result',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Expanded(child: SearchResultListView()),
         ],
       ),
+    );
+  }
+}
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 3),
+          child: BookListViewItem(),
+        );
+      },
     );
   }
 }
