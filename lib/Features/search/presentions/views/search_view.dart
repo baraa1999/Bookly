658f3ff2252c3d7bf43a795baf62dce13c9dad3f
baraa1999 <baraaalian7@gmail.com>
2023-08-24 14:19:22 +0300

    create search view

---
+++ lib/Features/search/presentions/views/search_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'widget/Search_View_Body.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(child: SearchViewBody()),
+    );
+  }
+}
--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/Features/home/presention/views/book_details_view.dart';
 import 'package:bookly/Features/home/presention/views/home_view.dart';
+import 'package:bookly/Features/search/presentions/views/search_view.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../presentions/views/splach_view.dart';
@@ -7,13 +8,17 @@ import '../../presentions/views/splach_view.dart';
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/BookDetailsView';
-
+  static const kSearchView = '/searchView';
   // GoRouter configuration
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) => const SearchView(),
       ),
       GoRoute(
         path: kHomeView,
